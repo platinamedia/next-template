800fb5d156d4bff2fce5f3721e0f2633dccda5dd
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-07-26 22:32:31 +0300

    chore: rename Page.ts file

--- src/types/Page.ts
+++ src/types/Page.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 import { NextPage } from "next";
 
-export type Page<T = {}> = NextPage & {
+export type Page<PageProps = {}> = NextPage<PageProps> & {
   title?: string;
   getLayout?: (page: React.ReactElement) => React.ReactElement;
 };
