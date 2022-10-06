fbc25420419930f2e844103fa94ad584b4a99415
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-10-06 23:14:12 +0300

    feat: create css file inside `pages` dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,12 @@ import { PrimaryLayout } from "@/layouts/PrimaryLayout";
 
 import { Page } from "@/types/Page";
 
+import styles from "./index.module.css";
+
 const HomePage: Page = () => {
   return (
     <div>
-      <h1>The Next.js template</h1>
+      <h1 className={styles.title}>The Next.js template</h1>
       <Link href="https://github.com/tsivinsky/next-template" passHref>
         <a>GitHub</a>
       </Link>
