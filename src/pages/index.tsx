c6004e009251ed69188d3b070a40bf84b212455f
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-10-12 22:15:20 +0300

    feat: update gh repo url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const HomePage: Page = () => {
   return (
     <div>
       <h1>The Next.js template</h1>
-      <Link href="https://github.com/tsivinsky/next-template" passHref>
+      <Link href="https://github.com/platinamedia/next-template" passHref>
         <a>GitHub</a>
       </Link>
     </div>
