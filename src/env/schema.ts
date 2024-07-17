66a36f9ebd0b02caef1d67a3c70804a83989826b
Daniil Tsivinsky <daniil@tsivinsky.com>
2024-07-17 19:46:16 +0300

    upd - comment env schema

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const clientSchema = z.object({
-  API_URL: z.string(),
+  // API_URL: z.string(),
 });
 
 export const clientEnv = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  // API_URL: process.env.NEXT_PUBLIC_API_URL,
 };
